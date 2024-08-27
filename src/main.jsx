408e5814f35d34edc61d39e03b8c727157f1348a
xandoalka <xandoalka@gmail.com>
2024-08-27 14:56:40 +0700

    ubah main

--- src/main.jsx
+++ src/main.jsx
@@ -30,8 +30,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
